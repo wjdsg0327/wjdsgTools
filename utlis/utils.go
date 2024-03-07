a8556ca549899bb095e9043a745a321e361eb8f3
wangjian <2268035794@qq.com>
2024-03-07 13:52:49 +0800

    暴露函数

--- utlis/utils.go
+++ utlis/utils.go
@@ -1,6 +1,7 @@
 package wjdsgtools
 
 import (
+	CalculateUtils "github.com/wjdsg0327/wjdsgtools/calculateUtils"
 	DateUtils "github.com/wjdsg0327/wjdsgtools/dateUtils"
 	IdUtils "github.com/wjdsg0327/wjdsgtools/idUtils"
 	Md5Utils "github.com/wjdsg0327/wjdsgtools/md5Utils"
@@ -17,33 +18,44 @@ func DateField() DateUtils.DateField {
 	return field
 }
 
+// DateUtils 日期处理
 func (Utils) DateUtils() DateUtils.DateUtils {
 	utils := DateUtils.DateUtils{}
 	return utils
 }
 
+// IdUtils id生成处理
 func (Utils) IdUtils() IdUtils.IdUtils {
 	utils := IdUtils.IdUtils{}
 	return utils
 }
 
+// RandomUtils 随机数处理
 func (Utils) RandomUtils() RandomUtils.RandomUtils {
 	utils := RandomUtils.RandomUtils{}
 	return utils
 }
 
+// Md5Utils 加处理
 func (Utils) Md5Utils() Md5Utils.Md5Utils {
 
 	utils := Md5Utils.Md5Utils{}
 	return utils
 }
 
+// StringUtils 字符串处理
 func (Utils) StringUtils() StringUtils.StringUtils {
 	utils := StringUtils.StringUtils{}
 	return utils
 }
 
+// StructUtils 结构体处理
 func (Utils) StructUtils() StructUtils.StructUtils {
 	utils := StructUtils.StructUtils{}
+	return utils
+}
+
+func (Utils) calculateUtils() CalculateUtils.CalculateUtils {
+	utils := CalculateUtils.CalculateUtils{}
 	return utils
 }
