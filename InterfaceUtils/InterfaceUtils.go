5b3419ef5c512592e5f5f085645a260c4c3f3b59
wangjian <2268035794@qq.com>
2024-03-08 16:22:56 +0800

    新增接口类型的方法

--- InterfaceUtils/InterfaceUtils.go
+++ InterfaceUtils/InterfaceUtils.go
@@ -1,1 +1,19 @@
 package InterfaceUtils
+
+import "reflect"
+
+type InterfaceUtils struct {
+}
+
+// GetType 获取接口实际类型
+func (InterfaceUtils) GetType(any interface{}) string {
+
+	r := reflect.TypeOf(any).Kind().String()
+
+	return r
+}
+
+// IsType 检查接口的类型是否为某个特定类型
+func (InterfaceUtils) IsType(i interface{}, targetType reflect.Kind) bool {
+	return reflect.TypeOf(i).Kind() == targetType
+}
